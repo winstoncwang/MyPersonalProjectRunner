f49e2c56892eee83843186ff907ee7b1de0cd844
winstoncwang <congwang@live.co.uk>
2020-05-02 13:29:49 +0100

    V 0.15
    use fsPromise.access to check file existence and throw error is necessary.

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 #! /usr/bin/env node
+const fs = require('fs');
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
 const program = require('caporal'); //caporal recommends this naming convention.
 //Caporal imports a program rather than a class or a object of methods.
 
 //program
-
 program
 	.version('0.1')
-	.argument('[filename]', 'Name of file to execute')
-	.action((args) => {
-		console.log(args);
+	.argument('[filename]', 'Name of file to execute') //[filename] optional set default to index.js
+	.action(async ({ filename }) => {
+		const name = filename || 'index.js';
+
+		try {
+			await fs.promises.access(name); //promise.acces to check if we have the privilage to access the file/check its existance.
+		} catch (err) {
+			throw new Error(`file not found: ${name}`);
+		}
+
+		const start = debounce(() => {
+			console.log('-----USER PROGRAM STARTED!!-----');
+		}, 300);
+
+		chokidar
+			.watch('.') //{ ignoreInitial: true }
+			.on('add', start)
+			.on('change', start)
+			.on('unlink', start);
 	});
 
 program.parse(process.argv);
-
-const start = debounce(() => {
-	console.log('-----USER PROGRAM STARTED!!-----');
-}, 300);
-
-chokidar
-	.watch('.') //{ ignoreInitial: true }
-	.on('add', start);
-// .on('change', () => {
-// 	process.stdout.write('file changed');
-// })
-// .on('unlink', () => {
-// 	process.stdout.write('file unlinked');
-// });
