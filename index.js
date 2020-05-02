d4bb413b16e7cdea7cd591de139e466b44f68176
winstoncwang <congwang@live.co.uk>
2020-05-02 10:50:53 +0100

    V 0.13
    lodash debounce added to user start program notification line

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 const chokidar = require('chokidar');
+const lodashDebounce = require('lodash.debounce');
+
+const start = debounce(() => {
+	console.log('USER PROGRAM STARTED!!');
+}, 100);
 
 chokidar
 	.watch('.') //{ ignoreInitial: true }
-	.on('add', () => {
-		process.stdout.write('file added');
-	})
+	.on('add', start)
 	.on('change', () => {
 		process.stdout.write('file changed');
 	})
 	.on('unlink', () => {
 		process.stdout.write('file unlinked');
 	});
-
-
-    const debounce = (func, wait)=>{
-
-        const timeOut = setTimeout(func,wait);
-
-    }
