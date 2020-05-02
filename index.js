6496cc542fd87e8247fa3444a5edc158b07320a2
winstoncwang <congwang@live.co.uk>
2020-05-02 11:59:38 +0100

    V 0.14
    caporal implemented with basic features

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 #! /usr/bin/env node
 const chokidar = require('chokidar');
-const lodashDebounce = require('lodash.debounce');
+const debounce = require('lodash.debounce');
+const program = require('caporal'); //caporal recommends this naming convention.
+//Caporal imports a program rather than a class or a object of methods.
+
+//program
+
+program
+	.version('0.1')
+	.argument('[filename]', 'Name of file to execute')
+	.action((args) => {
+		console.log(args);
+	});
+
+program.parse(process.argv);
 
 const start = debounce(() => {
-	console.log('USER PROGRAM STARTED!!');
-}, 100);
+	console.log('-----USER PROGRAM STARTED!!-----');
+}, 300);
 
 chokidar
 	.watch('.') //{ ignoreInitial: true }
-	.on('add', start)
-	.on('change', () => {
-		process.stdout.write('file changed');
-	})
-	.on('unlink', () => {
-		process.stdout.write('file unlinked');
-	});
+	.on('add', start);
+// .on('change', () => {
+// 	process.stdout.write('file changed');
+// })
+// .on('unlink', () => {
+// 	process.stdout.write('file unlinked');
+// });
